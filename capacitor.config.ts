52d53e1fd5681f50ed0c1caf5009e5d39e1eb310
Franklin RD <frivero.duharte@gmail.com>
2025-07-27 15:55:53 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,16 @@ const config: CapacitorConfig = {
   appId: 'com.techpromux.app',
   appName: 'App',
   webDir: 'dist/apps/app/browser',
+
+  /*
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+      style: 'DARK',
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  */
 };
 
 export default config;
